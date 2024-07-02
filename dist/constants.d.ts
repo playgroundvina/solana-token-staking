6173c653eedf6b794584e57608f100d8b2fd7bd2
Xiao Wu <baoanhiuh.dev@gmail.com>
2024-07-02 10:34:56 +0700

    update

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,6 +1,6 @@
 import * as anchor from "@coral-xyz/anchor";
 export declare const STAKE_DEPOSIT_RECEIPT_DISCRIMINATOR: number[];
-export declare const SPL_TOKEN_STAKING_ID = "STAKEGztX7S1MUHxcQHieZhELCntb9Ys9BgUbeEtMu1";
+export declare const SPL_TOKEN_STAKING_ID = "AA9TGQNQuJks86HSJjXVWDAotvK8XKatRE9t2PXkLoGg";
 export declare const STAKE_POOL_OWNER = "AL145KtKMxnRDfcruv61Kt4WL7FKtVYuqPA3nM8adWk";
 export declare const TOKEN_PROGRAM_ID: anchor.web3.PublicKey;
 export declare const RENT_PROGRAM_ID: anchor.web3.PublicKey;
